3ebc64119bf020c7f12ee57f3016c0af00d573e2
VIIBII <birlavishal90@gmail.com>
2023-07-06 07:46:08 +0530

    mq

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {},
     screens: {
-      xl: { min: "1400px", max: "1600px" },
+      xl: { min: "1001px", max: "1600px" },
       lg: { min: "800px", max: "1000px" },
       md: { max: "767px" },
       sm: { max: "550px" },
